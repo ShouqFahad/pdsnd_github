b76e8af6a21ad794871807fdd9524b9cbb0eed8d
ShouqFahad <Shougfahadm@gmail.com>
2023-02-11 03:37:32 +0300

    refactor: added comments on the code

--- Bikeshare_shouq.py
+++ Bikeshare_shouq.py
@@ -10,7 +10,7 @@ Created on Sat Jan 21 14:26:55 2023
 import time
 import pandas as pd
 import numpy as np
-
+#modify data location to run the code on your device 
 CITY_DATA = { 'Chicago': 'chicago.csv',
               'New York City': 'new_york_city.csv',
               'Washington': 'washington.csv' }
